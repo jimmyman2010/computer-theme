cb6f12af78e24626da45d7a803feda2775678949
jimmyman2010 <tranduyminhman@gmail.com>
2015-05-27 11:18:40 +0700

    fix menu

--- inc/left-menu-2.php
+++ inc/left-menu-2.php
@@ -6,36 +6,36 @@
             <li class="has-sub-menu accessories">
                 <a href="product_list.php" title="Máy bộ HP">Máy bộ HP<i class="fa fa-chevron-circle-down"></i></a>
                 <ul class="sub-menu">
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ HP DC 7900">Máy bộ HP DC 7900</a></li>
+                    <li><a href="product_list.php" title="Máy bộ HP DC 7900"><i class="fa fa-angle-double-right"></i>Máy bộ HP DC 7900</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ HP DC 7800">Máy bộ HP DC 7800</a></li>
+                    <li><a href="product_list.php" title="Máy bộ HP DC 7800"><i class="fa fa-angle-double-right"></i>Máy bộ HP DC 7800</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ HP DC 7700">Máy bộ HP DC 7700</a></li>
+                    <li><a href="product_list.php" title="Máy bộ HP DC 7700"><i class="fa fa-angle-double-right"></i>Máy bộ HP DC 7700</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ HP 6000 Pro">Máy bộ HP 6000 Pro</a></li>
+                    <li><a href="product_list.php" title="Máy bộ HP 6000 Pro"><i class="fa fa-angle-double-right"></i>Máy bộ HP 6000 Pro</a></li>
                 </ul>
             </li>
 
             <li class="has-sub-menu cases">
                 <a href="product_list.php" title="Máy bộ Dell">Máy bộ Dell<i class="fa fa-chevron-circle-down"></i></a>
                 <ul class="sub-menu">
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Optiplex 740">Máy bộ Dell Optiplex 740</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Optiplex 740"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Optiplex 740</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Optiplex 755">Máy bộ Dell Optiplex 755</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Optiplex 755"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Optiplex 755</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Optiplex 760">Máy bộ Dell Optiplex 760</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Optiplex 760"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Optiplex 760</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Optiplex 780">Máy bộ Dell Optiplex 780</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Optiplex 780"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Optiplex 780</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Optiplex 790">Máy bộ Dell Optiplex 790</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Optiplex 790"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Optiplex 790</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Optiplex 796">Máy bộ Dell Optiplex 796</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Optiplex 796"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Optiplex 796</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Vostro/Inpiron">Máy bộ Dell Vostro/Inpiron</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Vostro/Inpiron"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Vostro/Inpiron</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell XPS/Studio">Máy bộ Dell XPS/Studio</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell XPS/Studio"><i class="fa fa-angle-double-right"></i>Máy bộ Dell XPS/Studio</a></li>
 
-                    <li><i class="fa fa-angle-double-right"></i><a href="product_list.php" title="Máy bộ Dell Optiplex 380">Máy bộ Dell Optiplex 380</a></li>
+                    <li><a href="product_list.php" title="Máy bộ Dell Optiplex 380"><i class="fa fa-angle-double-right"></i>Máy bộ Dell Optiplex 380</a></li>
                 </ul>
             </li>
 
